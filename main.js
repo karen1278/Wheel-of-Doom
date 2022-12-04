c647122c9f69b25421fffd8e183072df4ad0c8ba
karen1278 <kjaneth111@gmail.com>
2022-12-04 14:22:46 -0500

    firts commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const entrada = document.querySelector("input")
+const añadir = document.querySelector(".btnadd")
+const ul = document.querySelector("ul")
+let elegido = document.querySelector("figcaption")
+
+añadir.addEventListener("click", (e) => {
+    e.preventDefault();
+    const text =entrada.value
+    if(text !==""){
+        const li = document.createElement("li")
+        const p = document.createElement("p")
+        p.textContent=text
+        li.appendChild(p)
+        li.appendChild(addDeleteBtn())
+        ul.appendChild(li)
+        entrada.value=""
+    } 
+});
+
+const addDeleteBtn = () =>{
+    const deleteBtn = document.createElement("button")
+    deleteBtn.textContent="x"
+    deleteBtn.className = "btnDelete"
+
+    deleteBtn.addEventListener("click", (e)=>{
+        const parentBtn = e.target.parentElement
+        ul.removeChild(parentBtn);
+        const nombrea = document.querySelectorAll("li")
+            if (nombres.leng === 0){
+                empty.style.display="block"
+            }
+    })
+    return deleteBtn
+}
