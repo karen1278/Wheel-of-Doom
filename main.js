68bc68dd5bbaec31be2faa4c277df178195cd557
dcstll <danicastillo789@gmail.com>
2022-12-07 11:39:32 -0500

    Adición de boton play

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-const entrada = document.querySelector("input")
-const añadir = document.querySelector(".btnadd")
-const ul = document.querySelector("ul")
-let elegido = document.querySelector("figcaption")
+ const entrada = document.querySelector("input")
+ const añadir = document.querySelector(".btnadd")
+ const ul = document.querySelector("ul")
+ let elegido = document.querySelector("figcaption")
 
-añadir.addEventListener("click", (e) => {
-    e.preventDefault();
-    const text =entrada.value
-    if(text !==""){
-        const li = document.createElement("li")
-        const p = document.createElement("p")
-        p.textContent=text
-        li.appendChild(p)
-        li.appendChild(addDeleteBtn())
-        ul.appendChild(li)
-        entrada.value=""
-    } 
-});
+ añadir.addEventListener("click", (e) => {
+     e.preventDefault();
+     const text =entrada.value
+     if(text !==""){
+         const li = document.createElement("li")
+         const p = document.createElement("p")
+         p.textContent=text
+         li.appendChild(p)
+         li.appendChild(addDeleteBtn())
+         ul.appendChild(li)
+         entrada.value=""
+     } 
+ });
 
-const addDeleteBtn = () =>{
-    const deleteBtn = document.createElement("button")
-    deleteBtn.textContent="x"
-    deleteBtn.className = "btnDelete"
+ const addDeleteBtn = () =>{
+     const deleteBtn = document.createElement("button")
+     deleteBtn.textContent="x"
+     deleteBtn.className = "btnDelete"
 
-    deleteBtn.addEventListener("click", (e)=>{
-        const parentBtn = e.target.parentElement
-        ul.removeChild(parentBtn);
-        const nombrea = document.querySelectorAll("li")
-            if (nombres.leng === 0){
-                empty.style.display="block"
+     deleteBtn.addEventListener("click", (e)=>{
+         const parentBtn = e.target.parentElement
+         ul.removeChild(parentBtn);
+         const nombres = document.querySelectorAll("li")
+             if (nombres.leng === 0){
+                 empty.style.display="block"
             }
-    })
-    return deleteBtn
-}
+     })
+     return deleteBtn
+ }
